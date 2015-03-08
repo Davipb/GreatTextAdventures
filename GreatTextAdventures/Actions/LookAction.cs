21d4ecce9f77d74529b76ba95f3148007918c067
Davipb <daviparca@gmail.com>
2015-03-08 13:04:41 -0300

    Improved 'Look' to support conflicting code names

--- GreatTextAdventures/Actions/LookAction.cs
+++ GreatTextAdventures/Actions/LookAction.cs
@@ -23,25 +23,42 @@ namespace GreatTextAdventures.Actions
 			if (string.IsNullOrWhiteSpace(action) || action == "around" || action == "room")
 			{
 				// Just look at the room, in general
-				Console.WriteLine("You are in a {0}.", GameSystem.CurrentMap.CurrentRoom.Describe());
+				Console.WriteLine("You are in a {0}", GameSystem.CurrentMap.CurrentRoom.Describe());
 			}
 			else
 			{
-				bool found = false;
+				Item[] found = GameSystem.CurrentMap.CurrentRoom.Items.Where(x => x.CodeNames.Contains(action)).ToArray();
 
-				foreach (var item in GameSystem.CurrentMap.CurrentRoom.Items)
-				{
-					if (item.CodeNames.Contains(action))
-					{
-						Console.WriteLine(item.Description);
-						found = true;
-						break;
-					}
-				}
-
-				if (!found)
+				if (found.Length == 0)
 				{
 					Console.WriteLine("There is no '{0}'.", action);
+				}
+				else if (found.Length > 1)
+				{
+					Console.WriteLine("There are multiple '{0}'. Please specify:", action);
+
+					for (int i = 0; i < found.Length; i++)
+					{
+						Console.WriteLine("{0}. {1}", i + 1, found[i].DisplayName);
+					}
+
+					while(true)
+					{
+						int pressed;
+
+						if (int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out pressed))
+						{
+							if (pressed > 0 && pressed <= found.Length)
+							{								
+								Console.WriteLine(found[pressed - 1].Description);
+								break;
+							}
+						}
+					}
+				}
+				else
+				{
+					Console.WriteLine(found[0].Description);
 				}
 			}
 			
