71bf425312e8c231e2477d0c223b6c137841f309
Davipb <daviparca@gmail.com>
2015-03-20 17:37:32 -0300

    Added random weapon generation

--- GreatTextAdventures/Items/Weapon.cs
+++ GreatTextAdventures/Items/Weapon.cs
@@ -1,4 +1,9 @@
-﻿namespace GreatTextAdventures.Items
+﻿using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace GreatTextAdventures.Items
 {
 	public class Weapon : Item
 	{
@@ -7,11 +12,24 @@
 		public static Weapon Random()
 		{
 			Weapon result = new Weapon();
-			result.DisplayName = "Sword";
-			result.CodeNames = new[] { "sword" };
-			result.Attack = 10;
-			result.Description = "A generic sword.";
-			
+			result.Attack = GameSystem.RNG.Next(1, 101);
+			StringBuilder weaponName = new StringBuilder();
+
+			JObject weapons = JObject.Parse(File.ReadAllText(@"Items\Weapons.json"));
+			JArray names = (JArray)weapons["Names"];
+			JArray modifiers = (JArray)weapons["Modifiers"];
+
+			JArray chosenModifier = (JArray)modifiers.OrderBy(x => GameSystem.RNG.Next()).First();
+			weaponName.Append(chosenModifier[0]);
+			result.Attack += (int)chosenModifier[1];
+
+			weaponName.Append(" ");
+
+			string chosenName = (string)names.OrderBy(x => GameSystem.RNG.Next()).First();
+			weaponName.Append(chosenName);
+			result.CodeNames = new[] { chosenName.ToLowerInvariant() };
+			result.DisplayName = weaponName.ToString();
+
 			return result;
 		}
 	}
