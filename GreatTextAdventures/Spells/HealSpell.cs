7c1b327edd56aee3e4cbe50ac91d1e3379ff8da1
Davipb <daviparca@gmail.com>
2015-03-21 10:45:55 -0300

    Added healing spell

---
+++ GreatTextAdventures/Spells/HealSpell.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GreatTextAdventures.People;
+
+namespace GreatTextAdventures.Spells
+{
+	public class HealSpell : GameSpell
+	{
+		const int HealPerLevel = 5;
+		const int CostPerLevel = 10;
+
+		private int level;
+
+		public override string DisplayName { get { return string.Format("Heal {0}", level); } }
+		public override IEnumerable<string> CodeNames
+		{
+			get 
+			{ 
+				yield return "heal";
+				yield return string.Format("heal {0}", level);
+			}
+		}
+		public override string Description
+		{
+			get { return string.Format("Heals {0} health", HealPerLevel * level); }
+		}
+		public override int Cost
+		{
+			get { return CostPerLevel * level; }
+		}
+
+		public override void Cast(Person caster, Person target)
+		{
+			if (caster.Mana < Cost)
+			{
+				Console.WriteLine("Not enough mana!");
+				return;
+			}
+
+			caster.Mana -= Cost;
+			target.Health += HealPerLevel * level;
+			Console.WriteLine("{0} was healed for {1} health", target.DisplayName, HealPerLevel * level);
+		}
+	}
+}
