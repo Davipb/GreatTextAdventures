8632a8b16c6e5ef4c526385f6e51a2a3baadd9d5
Davipb <daviparca@gmail.com>
2015-07-28 21:47:23 -0300

    Add locked chests

---
+++ GreatTextAdventures/Items/ChestKey.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace GreatTextAdventures.Items
+{
+	public class ChestKey : ILookable
+	{
+		public string DisplayName => "Chest Key";
+		public IEnumerable<string> CodeNames
+		{
+			get
+			{
+				yield return "chest key";
+				yield return "key";
+			}
+		}
+		public string Description => "Can be used to open a Locked Chest.";
+		public bool CanTake => true;
+
+		public void Update() { }
+	}
+}
---
+++ GreatTextAdventures/Items/LockedChest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatTextAdventures.Items
+{
+	public class LockedChest : LootChestItem
+	{
+		public bool Locked { get; private set; }
+
+		public override string DisplayName
+		{
+			get
+			{
+				if (Locked)
+					return "Locked Chest";
+				return base.DisplayName;
+			}
+		}
+
+		public override string Description
+		{
+			get
+			{
+				if (Locked)
+					return "It has a keyhole in the front.";
+				return base.Description;
+			}
+		}
+
+		public override void Open()
+		{
+			if (Locked)
+			{
+				if (GameSystem.Player.Inventory.OfType<ChestKey>().Any())
+				{
+					GameSystem.WriteLine($"{GameSystem.Player.DisplayName} unlocked the chest");
+					GameSystem.WriteLine("The key is now stuck in the keyhole and won't budge");
+
+					GameSystem.Player.Inventory.RemoveAt(
+						GameSystem.Player.Inventory.FindIndex(x => x is ChestKey));
+
+					Locked = false;
+
+					base.Open();
+				}
+				else
+				{
+					GameSystem.WriteLine("You don't have a key!");
+				}
+			}
+			else
+			{
+				base.Open();
+			}
+		}
+	}
+}
