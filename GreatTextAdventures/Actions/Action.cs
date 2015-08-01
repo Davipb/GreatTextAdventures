f106aa4d8da504d5bf69953af43c2fab198d6210
Davipb <daviparca@gmail.com>
2015-08-01 16:30:16 -0300

    Improved Action.ListItemPossibilities

--- GreatTextAdventures/Actions/Action.cs
+++ GreatTextAdventures/Actions/Action.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System;
 using GreatTextAdventures.Items;
 using GreatTextAdventures.People;
 using System.Linq;
@@ -27,6 +26,18 @@ namespace GreatTextAdventures.Actions
 		protected static void ListItemPossibilities(ILookable item)
 		{
 			List<GameAction> possible = new List<GameAction>();
+			possible.Add(GameSystem.Actions.Find(x => x is LookAction));
+
+			if (item.CanTake)
+			{
+				if (GameSystem.Player.Inventory.Contains(item))
+					possible.Add(GameSystem.Actions.Find(x => x is DropAction));
+				else
+					possible.Add(GameSystem.Actions.Find(x => x is TakeAction));
+			}
+
+			if (item as Spells.GameSpell != null)
+				possible.Add(GameSystem.Actions.Find(x => x is CastAction));
 
 			if (item as Person != null)
 			{
@@ -35,21 +46,16 @@ namespace GreatTextAdventures.Actions
 			}
 
 			if (item as Weapon != null)
-			{
 				possible.Add(GameSystem.Actions.Find(x => x is EquipAction));
-			}
 
 			if (item as IContainer != null)
-			{
 				possible.Add(GameSystem.Actions.Find(x => x is OpenAction));
-			}
 
 			if (item as IUsable != null)
-			{
 				possible.Add(GameSystem.Actions.Find(x => x is UseAction));
-			}
 
-			GameSystem.WriteLine(GameSystem.Enumerate(possible.Select(x => x.Aliases.First()), "Try", null, "", "or"));
+			if (possible.Any())
+				GameSystem.WriteLine(GameSystem.Enumerate(possible.Select(x => x.Aliases.First()), "Try", null, "", "or"));
 		}
 	}
 }
